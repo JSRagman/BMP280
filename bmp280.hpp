6ff6c85d722992f096b7a77b2d0ad18356471fc8
Chuck <42876386+JSRagman@users.noreply.github.com>
2018-09-07 14:50:03 -0400

    Update bmp280.hpp
    Moved data structures, format edits

--- bmp280.hpp
+++ bmp280.hpp
@@ -21,12 +21,12 @@
 #ifndef BMP280_HPP_
 #define BMP280_HPP_
 
-
 #include  <ctime>            // time_t
 #include  <mutex>            // std::mutex
 #include  <stdint.h>         // int16_t, uint16_t
 
 #include "bbb-i2c.hpp"       // I2CBus
+
 #include "bmp280_defs.hpp"
 #include "bmp280_comp.hpp"
 
@@ -35,97 +35,36 @@ using bbbi2c::I2CBus;
 namespace bosch_bmp280
 {
 
-/*
- * struct CalParams
- *
- * Description:
- *   A structure for BMP280 calibration parameters.
- *
- * Namespace:
- *   bosch_bmp280
- *
- * Header File(s):
- *   bmp280.hpp
- */
-struct CalParams
-{
-	uint16_t  t1;
-	 int16_t  t2;
-	 int16_t  t3;
-
-	uint16_t  p1;
-	 int16_t  p2;
-	 int16_t  p3;
-	 int16_t  p4;
-	 int16_t  p5;
-	 int16_t  p6;
-	 int16_t  p7;
-	 int16_t  p8;
-	 int16_t  p9;
-
-	bool loaded;
-
-	CalParams();
-};
-
-/*
- * struct TPData
- *
- * Description:
- *   A structure for recording temperature and pressure
- *   data as 32-bit integers.
- *
- *   Useful for:
- *     1.  raw temperature and pressure data, or
- *     2.  results from 32-bit fixed-point compensation.
- *
- * Namespace:
- *   bosch_bmp280
- *
- * Header File(s):
- *   bmp280.hpp
- */
-struct TP32Data
-{
-	 int32_t  temperature;
-	uint32_t  pressure;
-
-	TP32Data ( int32_t temp=0, uint32_t press=0 );
-};
-
-
 class BMP280
 {
+	protected:
+	
+		I2CBus*    i2cbus;
+		uint8_t    i2caddr;
+		int32_t    tfine;
+		CalParams  cparams;
 
-protected:
+	public:
+	
+		std::mutex mtx;
 
-	 I2CBus*  i2cbus;
-	uint8_t   i2caddr;
+		BMP280 ( I2CBus* bus, uint8_t addr );
+		~BMP280 ();
 
-	 int32_t    tfine;
-	 CalParams  cparams;
+		TP32Data  GetUncompData ();
+		TP32Data  GetComp32FixedData ();
 
-public:
+		void      LoadCalParams ();
+		int32_t   Comp32FixedTemp  (  int32_t unctemp  );
+		uint32_t  Comp32FixedPress ( uint32_t uncpress );
 
-	std::mutex mtx;
+		void  GetRegs   ( uint8_t startaddr, uint8_t* data, int len );
+		void  SetRegs   ( uint8_t* data, int len );
+	
+		void  SetConfig ( int preset );
+		void  SetConfig ( uint8_t ctrl, uint8_t conf );
 
-	BMP280 ( I2CBus* bus, uint8_t addr );
-	~BMP280 ();
-
-	TP32Data  GetUncompData ();
-
-	void      LoadCalParams ();
-	int32_t   Comp32FixedTemp  (  int32_t unctemp  );
-	uint32_t  Comp32FixedPress ( uint32_t uncpress );
-
-	TP32Data  GetComp32FixedData ();
-
-	void  GetRegs   ( uint8_t startaddr, uint8_t* data, int len );
-	void  SetRegs   ( uint8_t* data, int len );
-	void  SetConfig ( int preset );
-	void  SetConfig ( uint8_t ctrl, uint8_t conf );
-
-	void  Reset ();
+		void  Reset ();
 
 }; // class BMP280
 
